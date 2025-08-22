543d8631ae9f957d465fc113670972fb74b72c17
Streamtg <202310475@issu.edu.do>
2025-08-22 12:34:38 -0400

    Update config.py

--- config.py
+++ config.py
@@ -36,7 +36,7 @@ class Config(object):
     # digital_botz client config
     API_ID = os.environ.get("API_ID", "10565113")
     API_HASH = os.environ.get("API_HASH", "d2220b87fb12fc430dc8fcebbb03d95c")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "8256526472:AAHyvbxwrK1Z8_CcU9p4Odh6y6twjJKEzhc") 
 
     # premium account string session required 😢 
     STRING_SESSION = os.environ.get("STRING_SESSION", "")
